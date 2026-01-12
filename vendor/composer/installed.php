b026dd24016c223e55de97c31becca5ca4291147
Milan Ricoul <mricoul@beapi.fr>
2026-01-12 16:54:20 +0000

    Release version 1.1.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'beapi/acf-palette',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ac790af9a8db819be44656a0b872f8c1161cc9d4',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'beapi/acf-palette' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ac790af9a8db819be44656a0b872f8c1161cc9d4',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'composer/installers' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
