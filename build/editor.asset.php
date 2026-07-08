2eba35cb623f9288f009ee134143e8f5c67fb699
Milan Ricoul <mricoul@beapi.fr>
2026-07-08 13:31:27 +0000

    Release version 1.1.3

---
+++ build/editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ee95dbd5f59b0df8871f');
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '4e27bb6a3fbfb6e19b5f');
